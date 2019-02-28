d9d6ac864849d3006f39360963cd27887703931c
BonnieCoelho <jboni2014@gmail.com>
2019-02-28 16:02:13 -0300

    Edgar
    Produção in progress

--- Assets/Scripts/Edgar/EdgarAction.cs
+++ Assets/Scripts/Edgar/EdgarAction.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class EdgarAction : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private float speed;
+    private float horizontalspeed;
+    private Animator anim;
+    bool andar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        speed = 4.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+    }
+    void FixedUpdate()
+    {
+        Walk();
+        Ataques();
+    }
+    void Walk()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+
+        /*if (Input.GetKeyDown(KeyCode.UpArrow) && timeJump < 2 || Input.GetKeyDown(KeyCode.W) && timeJump < 2)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            timeJump++;
+            isGrounded = false;
+        }*/
+
+    }
+    void Ataques()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            anim.SetBool("Ataque", true);
+        }
+        else
+        {
+            anim.SetBool("Ataque", false);
+        }
     }
 }
---
+++ Assets/Scripts/Minotauro/ActionMinotauro.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMinotauro : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D Minotaurorb;
+    private Animator anim;
+    public bool BoneAttack;
+    public bool RushAttack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Minotaurorb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        StartCoroutine(AtaqueOsso());
+        BoneAttack = false;
+        RushAttack = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+    IEnumerator AtaqueOsso()
+    {
+      yield return new WaitForSeconds(3f);
+        BoneAttack = true;
+        if(BoneAttack == true)
+        {
+            GetComponent<Animator>().SetBool("Atk1", true);
+        }
+        yield return new WaitForSeconds(6f);
+        BoneAttack = false;
+        if(BoneAttack == false)
+        {
+            GetComponent<Animator>().SetBool("Atk1", false);
+        }
+        StartCoroutine(AtaqueCorrendo());
+    }
+    IEnumerator AtaqueCorrendo()
+    {
+        yield return new WaitForSeconds(3f);
+        RushAttack = true;
+        if(RushAttack == true)
+        {
+            GetComponent<Animator>().SetBool("Atk2", true);
+        }
+        yield return new WaitForSeconds(6f);
+        RushAttack = false;
+        if (RushAttack == false)
+        {
+            GetComponent<Animator>().SetBool("Atk2", false);
+        }
+        StopCoroutine(AtaqueCorrendo());
+        yield return new WaitForSeconds(2.0f);
+        StartCoroutine(AtaqueOsso());
+    }
+
+}
